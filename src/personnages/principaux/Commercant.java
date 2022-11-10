12ec38317c270bc4426c19ee0603e7fd46de3cdc
smart lenovo <smart lenovo@172.17.153.200>
2022-11-10 17:33:06 +0100

    m

--- src/personnages/principaux/Commercant.java
+++ src/personnages/principaux/Commercant.java
@@ -1,5 +1,29 @@
 package personnages.principaux;
 
-public class Commercant {
+import personnages.Humain;
+
+public class Commercant extends Humain {
+	public Commercant(String nom,int argent) {
+		super(nom,"thé",argent);
+	}
+	
+	
+	
+	public int seFaireExtorquer() {
+		int valeur=getArgent();
+		perdreArgent(valeur);
+		System.out.println("le monde est vraiment trop injuste");
+		return valeur;
+			
+	}	
+	
+
+	public void recevoir(int argent) {
+	
+		this.ajouterArgent(argent);
+		System.out.println("Merci infiniment");
+		
+		
+	}
 
 }
